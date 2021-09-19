94f7bc15a585c216a416d1d08c7c4a0bc18d5e6b
Agustin Leguizamon <agustin@pop-os.localdomain>
2021-09-19 13:59:25 -0300

    Clase 02 - Desafio

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Clase 02 - Desafio: Crear la App utilizando el CLI
         </p>
         <a
           className="App-link"

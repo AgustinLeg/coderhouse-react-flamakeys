fccf809b2670bf988997bef64c12b0ee109a8c78
Agustin Leguizamon <agustin@pop-os.localdomain>
2021-10-14 20:18:30 -0300

    Carga de items en ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import NavBar from "./components/NavBar/NavBar";
-import Spinner from "./components/Stateless/Spinner/Spinner";
-import productos from "./data/productos";
+
 
 function App() {
   const [items, setItems] = useState([]);
   const [cantidadCarrito, setCantidadCarrito] = useState(0);
-
-  useEffect(() => {
-    const getItems = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(productos);
-      }, 1000);
-    });
-    getItems.then((res) => {
-      setItems(res);
-    });
-  }, [items]);
 
   const addCantidadCarrito = (cantidad) => {
     setCantidadCarrito(cantidadCarrito + cantidad);
@@ -33,10 +21,10 @@ function App() {
       </header>
       <Switch>
         <Route exact path="/">
-          {items.length > 0 ? <ItemListContainer items={items} /> : <Spinner />}
+          <ItemListContainer items={items} setItems={setItems} />
         </Route>
         <Route exact path="/categoria/:nombre">
-          <ItemListContainer items={items} />
+          <ItemListContainer items={items} setItems={setItems}/>
         </Route>
         <Route exact path="/producto/:id">
           <ItemDetailContainer
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ItemList from "../ItemList/ItemList";
+import productos from "../../data/productos";
+import Spinner from "../Stateless/Spinner/Spinner";
 
-const ItemListContainer = ({ items }) => {
+const ItemListContainer = ({ items, setItems}) => {
   const { nombre } = useParams();
 
   const [itemsfilter, setItemsFilter] = useState([]);
+  
   useEffect(() => {
+      const getItems = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(productos);
+        }, 1000);
+      });
+      getItems.then((res) => {
+        setItems(res);
+      });
+  
     const filtrarProductos = (productos) => {
       let filter;
       switch (nombre) {
@@ -24,19 +36,23 @@ const ItemListContainer = ({ items }) => {
       }
     };
     filtrarProductos(items);
-  }, [nombre, items]);
+  }, [items,nombre,setItems]);
   return (
     <section>
-      <div className="container mt-5 pt-5 ">
-        <h2 className="text-center my-5">
-          {nombre ? nombre.toUpperCase() : "LAS MEJORES MARCAS AL MEJOR PRECIO"}
-        </h2>
-        <div className="productos">
-          <div className="item-list">
-            <ItemList items={itemsfilter} />
-          </div>
-        </div>
-      </div>
+      {items.length > 0
+            ?<div className="container mt-5 pt-5 ">
+                <h2 className="text-center my-5">
+                  {nombre ? nombre.toUpperCase() : "LAS MEJORES MARCAS AL MEJOR PRECIO"}
+                </h2>
+                <div className="productos">
+                  <div className="item-list">
+                    <ItemList items={itemsfilter} />
+                  </div>
+                </div>
+              </div>
+              : <Spinner />
+      }
+
     </section>
   );
 };
